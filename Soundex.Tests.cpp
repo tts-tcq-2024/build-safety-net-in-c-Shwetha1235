1e1c5d87158192ad8db86fd1d0744d4e32df3da8
Shwetha1235 <shwethaam333@gmail.com>
2024-09-06 10:24:23 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -4,6 +4,20 @@
 TEST(SoudexTestsuite, ReplacesConsonantsWithAppropriateDigits) {
  //AAA
   char soundex[5];
-  generateSoundex("AX", soundex);
- // ASSERT_EQ(soundex,"A200");
+ 
+ generateSoundex("AX", soundex);
+ ASSERT_STREQ(soundex,"A200");
+
+ generateSoundex("soundex", soundex);
+ ASSERT_STREQ(soundex,"S532");
+
+ generateSoundex("Robbert", soundex);
+ ASSERT_STREQ(soundex,"R163");
+
+ generateSoundex("R", soundex);
+ ASSERT_STREQ(soundex,"R000");
+
+ generateSoundex("aou", soundex);
+ ASSERT_STREQ(soundex,"A000");
+ 
 }
